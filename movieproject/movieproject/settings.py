54206ef44da4927abc592772da97703c994d4dbc
JungTag <dbsdudxor121@naver.com>
2021-07-17 14:36:27 +0900

    Set Static directories

--- movieproject/movieproject/settings.py
+++ movieproject/movieproject/settings.py
@@ -12,10 +12,17 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'movie', 'static'),
+    os.path.join(BASE_DIR, 'account', 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -56,7 +63,9 @@ ROOT_URLCONF = 'movieproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'modelproject/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
