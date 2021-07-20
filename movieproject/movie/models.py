25bc38fa9f534510c0e136ab8163039900758bd9
JungTag <dbsdudxor121@naver.com>
2021-07-20 16:04:11 +0900

    Add Staff Class && Edit detail

--- movieproject/movie/models.py
+++ movieproject/movie/models.py
@@ -3,19 +3,27 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Movie(models.Model):
-    title_kor = models.CharField(max_length=200)
-    title_eng = models.CharField(max_length=200)
-    postr_url = models.CharField(max_length=200, null=True)
+    title_kor = models.CharField(max_length=100)
+    title_eng = models.CharField(max_length=100)
+    poster_url = models.CharField(max_length=255, null=True)
     rating_aud = models.CharField(max_length=10)
     rating_cri = models.CharField(max_length=10)
     rating_net = models.CharField(max_length=10)
     genre = models.CharField(max_length=100)
     showtimes = models.CharField(max_length=10)
     release_date = models.CharField(max_length=20)
-    rate = models.CharField(max_length=200)
+    rate = models.CharField(max_length=100)
     summary = models.TextField()
-    staff = models.CharField(max_length=200)
+    # staff = models.CharField(max_length=200)
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+class Staff(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=50)
+    image_url = models.CharField(max_length=255, null=True)
+
+
